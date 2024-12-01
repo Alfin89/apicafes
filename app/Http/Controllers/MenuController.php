3aab0a1c782ab4fe63fe57cd7930957bd443f51d
Alfin89 <rebahansantuy30@gmail.com>
2024-12-01 18:56:12 +0700

    membuat Controller untuk menu

---
+++ app/Http/Controllers/MenuController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Menu;
+use Illuminate\Http\Request;
+
+class MenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Menu::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required',
+        ]);
+
+        $menu = Menu::create($validated);
+        return response()->json($menu, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Menu $menu)
+    {
+        return $menu;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Menu $menu)
+    {
+        $validated = $request->validate([
+            'name' => 'sometimes|required',
+            'price' => 'sometimes|required|numeric',
+            'category' => 'sometimes|required',
+        ]);
+
+        $menu->update($validated);
+        return response()->json($menu, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Menu $menu)
+    {
+        $menu->delete();
+        return response()->json(null, 204);
+    }
+}
